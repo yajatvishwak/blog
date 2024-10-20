4b32c748f22813035bf1bf8f84bf20f0f5c5c142
yajatvishwakk <yajat472001@gmail.com>
2024-10-20 14:54:34 +0530

    bruh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,11 @@
 </script>
 
 <div class="flex flex-col gap-4 mt-12">
-  <img class="ww-full" src="yajat.jpg" alt="" srcset="" />
-  <div class="text-3xl text-center lg:text-left">
+  <div class="text-3xl lg:text-left">
     <span class="opacity-55">I am a</span>
     <span class="opacity-100">23 year old programmer</span>
     <span class="opacity-55">from Bangalore, India.</span>
-    <span class="opacity-55">I work as</span>
+    <span class="opacity-55">I work as an</span>
     <span>Associate Software Engineer</span>
     <span class="opacity-55">at</span>
     <span class="opacity-55">Eurofins IT solutions.</span>
@@ -20,5 +19,6 @@
       >I love researching and tinkering with technology.</span
     >
   </div>
-  <div class="text-center lg:text-left">- Yajat Vishwakarma</div>
+  <div class="">- Yajat Vishwakarma</div>
+  <img class="lg:w-1/3" src="yajat.jpg" alt="" srcset="" />
 </div>
