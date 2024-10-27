75c1c5ec4b69f39760f69cec7df19e4ec10e7bc8
yajatvishwakk <yajat472001@gmail.com>
2024-10-27 08:44:28 +0530

    lessgo

--- scripts/handleImages.js
+++ scripts/handleImages.js
@@ -3,10 +3,14 @@ import uploadToR2 from "./uploadToR2.js";
 import path from "path";
 
 const imageRegex = /!\[.*?\]\((.*?)\)/g;
+const urlRegex = /^(https?:\/\/[^\s/$.?#].[^\s]*)$/i;
 function handleImages(fileContent) {
   let match;
   while ((match = imageRegex.exec(fileContent)) !== null) {
     let imagePath = match[1];
+    if (urlRegex.test(imagePath)) {
+      continue; // Skip if it's a URL
+    }
     let imageFilename = imagePath.substring(imagePath.lastIndexOf("/") + 1);
     const imageDirectoryPath = path.join("upload", "images");
 
