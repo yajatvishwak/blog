db9d0aad910c7aada3f86607f63c73f7563c5598
yajatvishwakk <yajat472001@gmail.com>
2024-10-30 14:31:57 +0530

    update tests

---
+++ README.md
@@ -0,0 +1,6 @@
+- upload blog markdown files to upload folder
+- respect the structure, do not remove blogs, images, purpose.md or whatsnew.md
+
+TODO:
+
+- Automatically update Whats new, thinking of sending changed files to a new worker on cloudflare that will summarize in points what are the new additions in the markdowns
--- test/checkblogfiles.test.js
+++ test/checkblogfiles.test.js
@@ -1,11 +1,25 @@
 import { describe, it, expect } from "vitest";
 import path from "path";
 import checkBlogFiles from "../scripts/checkblogfiles.js";
+import fs from "fs";
 
 describe("checkBlogFiles", () => {
   it("checking blog ids, dates, tags", () => {
     const blogDirectoryPath = path.join(__dirname, "..", "upload", "blogs");
     const result = checkBlogFiles(blogDirectoryPath);
     expect(result).toBe(true);
+  });
+  it("checks for existence of purpose.md and whatsnew.md files", () => {
+    const uploadDirectoryPath = path.join(__dirname, "..", "upload");
+
+    const purposeFileExists = fs.existsSync(
+      path.join(uploadDirectoryPath, "purpose.md")
+    );
+    const whatsNewFileExists = fs.existsSync(
+      path.join(uploadDirectoryPath, "whatsnew.md")
+    );
+
+    expect(purposeFileExists).toBe(true);
+    expect(whatsNewFileExists).toBe(true);
   });
 });
