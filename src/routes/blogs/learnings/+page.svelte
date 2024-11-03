e749e149bfef3ec7d20b6071ebde82501c44fe12
yajatvishwakk <yajat472001@gmail.com>
2024-11-03 18:00:03 +0530

    learnings

--- src/routes/blogs/learnings/+page.svelte
+++ src/routes/blogs/learnings/+page.svelte
@@ -3,27 +3,28 @@
   import BlogHomePage from "../../../lib/components/BlogHomePage.svelte";
   $subrouteName = "blogs";
   $lastVisited = "learnings";
+  export let data;
 
-  let listOfBlogs = {
-    2024: [
-      {
-        blogId: "blogId",
-        blogTitle: "Find Max depth of a tree",
-        tags: ["leetcode"],
-        words: 233,
-        date: "May 4th, 2023",
-      },
-    ],
-    2023: [
-      {
-        blogId: "blogId",
-        blogTitle: "Find Max depth of a tree",
-        tags: ["leetcode"],
-        words: 233,
-        date: "May 4th, 2023",
-      },
-    ],
-  };
+  // let listOfBlogs = {
+  //   2024: [
+  //     {
+  //       blogId: "blogId",
+  //       blogTitle: "Find Max depth of a tree",
+  //       tags: ["leetcode"],
+  //       words: 233,
+  //       date: "May 4th, 2023",
+  //     },
+  //   ],
+  //   2023: [
+  //     {
+  //       blogId: "blogId",
+  //       blogTitle: "Find Max depth of a tree",
+  //       tags: ["leetcode"],
+  //       words: 233,
+  //       date: "May 4th, 2023",
+  //     },
+  //   ],
+  // };
 </script>
 
-<BlogHomePage {listOfBlogs}></BlogHomePage>
+<BlogHomePage listOfBlogs={data}></BlogHomePage>
